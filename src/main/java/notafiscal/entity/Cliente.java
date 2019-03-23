57c94acf34ee82b9ed192d73a67a571c7501d065
Manoel Bento <manoelbento@MacBook-Air-de-Manoel-2923.local>
2019-03-23 11:33:21 -0300

    Adicionado a classe cliente

---
+++ src/main/java/notafiscal/entity/Cliente.java
@@ -0,0 +1,29 @@
+package notafiscal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+
+@Data
+public class Cliente {
+
+    @Id
+    @Column(name = "id_customer")
+    private Integer id;
+
+    @Column(name = "name")
+    private String nome;
+
+    @Column(name = "address")
+    private String endereco;
+
+    @Column(name = "country")
+    private String pais;
+
+}
