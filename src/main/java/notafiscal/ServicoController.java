702c9e8307d54f8000223deba994a5ffbb65f4fa
Jhony Almeida <jhony@jhony-mac.local>
2019-03-23 11:24:21 -0300

    movida entidade para pacote específico

--- src/main/java/notafiscal/ServicoController.java
+++ src/main/java/notafiscal/ServicoController.java
@@ -9,6 +9,7 @@ import io.micronaut.configuration.hibernate.jpa.scope.CurrentSession;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.spring.tx.annotation.Transactional;
+import notafiscal.entity.Servico;
 
 @Controller("/servicos")
 public class ServicoController {
