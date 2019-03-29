647dbebe1dc45e3003351421915c0f70d68f03ca
Jhony Almeida <jhony@jhony-mac.local>
2019-03-29 00:49:12 -0300

    Geração do PDF, template exemplo

--- src/main/java/notafiscal/MainController.java
+++ src/main/java/notafiscal/MainController.java
@@ -1,11 +1,25 @@
 package notafiscal;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.inject.Inject;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.server.types.files.StreamedFile;
+
 import notafiscal.entity.Fatura;
 
 @Controller("/notas-fiscais")
@@ -28,6 +42,45 @@ public class MainController {
     @Get("/{numero}")
     public Fatura findAll(Long numero) {
         return cassandraService.carregarFatura(numero).orElse(null);
+    }
+
+    @Get(value = "/{numero}/documento", produces = "application/pdf")
+    
+    public StreamedFile download(Long numero) {
+        Optional<Fatura> fatura = cassandraService.carregarFatura(numero);
+
+        if (!fatura.isPresent()) {
+            return null;
+        }
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+    
+        Context context = new Context();
+        context.setVariable("fatura", fatura.get());
+        
+        final String html = templateEngine.process("pdf_template", context);
+
+        InputStream inputStream = null;
+        try {
+            File tempFile = File.createTempFile(UUID.randomUUID().toString(), ".pdf");
+            FileOutputStream output = new FileOutputStream(tempFile);
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(html);
+            renderer.layout();
+            renderer.createPDF(output, true);
+            output.flush();
+            output.close();
+            inputStream = new FileInputStream(tempFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new StreamedFile(inputStream, "nota_fiscal.pdf");
     }
 
 }
